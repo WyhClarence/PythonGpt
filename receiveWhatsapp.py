4fcfa63bd30132d4bb35047f555c8d4a498529cb
Wyh <1239658231@qq.com>
2024-12-07 11:55:53 +0800

    1、新的.py，測試接收WhatsApp消息

--- receiveWhatsapp.py
+++ receiveWhatsapp.py
@@ -6,7 +6,6 @@ new_app1_bp = Blueprint('new_app1', __name__)
 @new_app1_bp.route('/receiveMsg', methods=['POST'])
 def new_app1_route(json):
     logger.debug(f"receiveMsg: {json}")
-    return "This is a route from new_app1"
 
 def init_app(app):
     app.register_blueprint(new_app1_bp)
