3ec5f3cb2456f815f72f2525180394780e81fb0b
Wyh <1239658231@qq.com>
2024-12-07 11:46:39 +0800

    1、新的.py，測試

--- receiveWhatsapp.py
+++ receiveWhatsapp.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 
 new_app1_bp = Blueprint('new_app1', __name__)
 
-@new_app1_bp.route('/new_app1')
+@new_app1_bp.route('/new_app1', methods=['GET'])
 def new_app1_route():
     return "This is a route from new_app1"
 
